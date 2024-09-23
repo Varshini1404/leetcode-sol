d662de5c24e5d348e0b1544a9bda800dafed2641
Varshini S <126443395+Varshini1404@users.noreply.github.com>
2024-09-23 11:56:51 +0530

    Time: 2 ms (96.55%), Space: 41.6 MB (69.3%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.java
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -1,32 +1,28 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        int num=1;
-        int l1=1;
-        int l2=1;
-        int l3=1;
-
-        int[] arr=new int[n+1];
-        arr[1]=1;
-
-        for(int i=2;i<n+1;i++)
-        {
-            num = Math.min(arr[l1] * 2, Math.min(arr[l2] * 3, arr[l3] * 5));
-            arr[i]=num;
-
-        if(num==arr[l1]*2)
-        {
-           l1++; 
+        int[] uglyNumbers = new int[n];
+        uglyNumbers[0] = 1;
+        int i2 = 0, i3 = 0, i5 = 0;
+        
+        int nextMultipleOf2 = 2;
+        int nextMultipleOf3 = 3;
+        int nextMultipleOf5 = 5;
+        for (int i = 1; i < n; i++) {
+            int nextUglyNumber = Math.min(nextMultipleOf2, Math.min(nextMultipleOf3, nextMultipleOf5));
+            uglyNumbers[i] = nextUglyNumber;
+            if (nextUglyNumber == nextMultipleOf2) {
+                i2++; 
+                nextMultipleOf2 = uglyNumbers[i2] * 2;
+            }
+            if (nextUglyNumber == nextMultipleOf3) {
+                i3++;
+                nextMultipleOf3 = uglyNumbers[i3] * 3;
+            }
+            if (nextUglyNumber == nextMultipleOf5) {
+                i5++;
+                nextMultipleOf5 = uglyNumbers[i5] * 5;
+            }
         }
-        if(num==arr[l2]*3)
-        {
-            l2++;
-        }
-        if(num==arr[l3]*5)
-        {
-            l3++;
-        }
-        }
-    
-    return num;
+        return uglyNumbers[n - 1];
     }
 }
