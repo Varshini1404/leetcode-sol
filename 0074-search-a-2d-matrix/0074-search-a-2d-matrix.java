e24e7ca1b04ebf45ff89acc1a4ee4d97f70ef059
Varshini S <126443395+Varshini1404@users.noreply.github.com>
2024-09-24 11:52:29 +0530

    Time: 0 ms (100%), Space: 42 MB (68.18%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        for(int i=0;i<matrix.length;i++)
+        //linear Search
+      /*  for(int i=0;i<matrix.length;i++)
         {
             for(int j=0;j<matrix[0].length;j++)
             {
@@ -10,7 +11,28 @@ class Solution {
                 }
             }
         }
-        return false;
+        return false;*/
+        int row=matrix.length;
+        int col=matrix[0].length;
+        int l=0;
+        int r=row*col-1;
+       
+        
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            int me = matrix[mid / col][mid % col];
+
+            if (me == target) {
+                return true; 
+            }
+            if (target < me) {
+                r = mid - 1; // Update r to search the left half
+            } else {
+                l = mid + 1; // Update l to search the right half
+            }
+        }
+        return false; 
 
     }
 }
