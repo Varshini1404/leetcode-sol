f3ae2cf4f6d7d9436cf8f83cee3fbb7cbd10dda0
Varshini S <126443395+Varshini1404@users.noreply.github.com>
2024-10-18 06:48:41 +0530

    Time: 8 ms (86%), Space: 40.7 MB (92.4%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.java
@@ -3,31 +3,26 @@ class Solution {
     int count = 0;
 
     public int countMaxOrSubsets(int[] nums) {
-        // Step 1: Calculate the maximum possible OR value
+       
         for (int num : nums) {
-            maxOR |= num;  // This gets the maximum possible OR of the entire array
+            maxOR |= num;  
         }
 
-        // Step 2: Use backtracking to count subsets with maximum OR
         backtrack(nums, 0, 0);
         
         return count;
     }
 
-    // Backtracking function
     private void backtrack(int[] nums, int index, int currentOR) {
         if (index == nums.length) {
-            // If we've reached the end of the array, check if the OR equals maxOR
             if (currentOR == maxOR) {
-                count++;  // Increment the count if current OR is the maximum
+                count++;  
             }
             return;
         }
 
-        // Include the current number in the subset
         backtrack(nums, index + 1, currentOR | nums[index]);
 
-        // Exclude the current number from the subset
         backtrack(nums, index + 1, currentOR);
     }
 }
