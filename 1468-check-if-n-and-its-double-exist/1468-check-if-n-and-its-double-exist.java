4fcb5024dffe403d820cef7fdf15f9f0117f5e62
Varshini S <varshinis.22cse@kongu.edu>
2024-12-01 12:20:50 +0530

    Time: 5 ms (8.99%), Space: 43.1 MB (33.23%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
@@ -1,24 +1,28 @@
+import java.util.Arrays;
+
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        for(int i=0;i<arr.length;i++)
-        {
-            for(int j=0;j<arr.length;j++)
-            {
-                if(i==j)
-                {
-                    continue;
-                }
-                else
-                {
-                    if(arr[i]==2*arr[j])
-                    {
-                        return true;
-                    }
-                }
+        Arrays.sort(arr);
 
+        for (int i = 0; i < arr.length; i++) {
+            int start = 0;         
+            int end = arr.length - 1; 
+
+            while (start <= end) {
+                int mid = start + (end - start) / 2; 
+                
+               
+                if (arr[mid] == 2 * arr[i] && mid != i) {
+                    return true; 
+                } 
+                else if (arr[mid] > 2 * arr[i]) {
+                    end = mid - 1;
+                } 
+                else {
+                    start = mid + 1;
+                }
             }
-
         }
-        return false;
+        return false; 
     }
 }
