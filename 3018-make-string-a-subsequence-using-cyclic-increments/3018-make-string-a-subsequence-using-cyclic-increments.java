4697ca3ef9984ae493212cf3c8a44d254a82c06b
Varshini S <varshinis.22cse@kongu.edu>
2024-12-05 03:14:05 +0530

    Time: 6 ms (70.31%), Space: 45.6 MB (54.14%) - LeetHub

---
+++ 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canMakeSubsequence(String str1, String str2) {
+        int i = 0, j = 0;
+
+        while (i < str1.length() && j < str2.length()) {
+            char c1 = str1.charAt(i);
+            char c2 = str2.charAt(j);
+
+            if (c1 == c2) {
+                i++;
+                j++;
+            } else {
+                char nextChar = (char) ((c1 - 'a' + 1) % 26 + 'a');
+                if (nextChar == c2) {
+                    i++;
+                    j++;
+                } else {
+                    i++; 
+                }
+            }
+        }
+
+        return j == str2.length();
+    }
+}
