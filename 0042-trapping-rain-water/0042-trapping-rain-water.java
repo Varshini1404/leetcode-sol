c59a28891ff8c26df7d4cfc263f94c85e849587c
Varshini S <varshinis.22cse@kongu.edu>
2025-02-09 21:32:53 +0530

    Time: 1 ms (64.69%), Space: 46.1 MB (80.39%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -4,18 +4,16 @@ class Solution {
         int[] left=new int[n];
         int[] right=new int[n];
         int tot=0;
-
+        
         left[0]=height[0];
         for(int i=1;i<n;i++){
             left[i]=Math.max(height[i],left[i-1]);
         }
-
         right[n-1]=height[n-1];
         for(int i=n-2;i>=0;i--)
         {
             right[i]=Math.max(height[i],right[i+1]);
         }
-
         for(int i=0;i<n;i++)
         {
             int water=Math.min(left[i],right[i]);
