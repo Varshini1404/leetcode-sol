cf7c26e07a842d29ec10fc39e2d93d576b73a53c
Varshini S <varshinis.22cse@kongu.edu>
2025-04-14 16:53:30 +0530

    Time: 475 ms (85.23%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,4 +1,5 @@
-select w1.id
-from weather as w1,weather as w2
-where datediff(w1.recorddate,w2.recorddate) = 1
-and w1.temperature > w2.temperature; 
+
+SELECT w1.id
+FROM weather AS w1, weather AS w2
+WHERE DATEDIFF(w1.recorddate, w2.recorddate) = 1
+  AND w1.temperature > w2.temperature;
