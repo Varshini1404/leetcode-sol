b56392a1d6a5509a06c07544c0e164bb1285723b
Varshini S <varshinis.22cse@kongu.edu>
2025-04-17 17:43:01 +0530

    Time: 0 ms (100%), Space: 42.3 MB (42.82%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,22 +1,22 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i=m-1;//last element nums1
-        int j=n-1;//last element nums2
-        int k=m+n-1;//last position
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
 
         while(i>=0 && j>=0)
         {
-        if(nums1[i]>nums2[j])
-        {
-            nums1[k]=nums1[i];
-            i--;
-        }
-        else
-        {
-            nums1[k]=nums2[j];
-            j--;
-        }
-        k--;
+            if(nums1[i]<nums2[j])
+            {
+                nums1[k]=nums2[j];
+                j--;
+            }
+            else
+            {
+                nums1[k]=nums1[i];
+                i--;
+            }
+            k--;
         }
         while(j>=0)
         {
@@ -24,7 +24,7 @@ class Solution {
             k--;
             j--;
         }
-
+        
+       
     }
-
 }
