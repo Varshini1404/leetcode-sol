537f968b763290d25c67d5e8dc89d4ce69274c8e
Varshini S <varshinis.22cse@kongu.edu>
2025-04-26 17:11:39 +0530

    Time: 15 ms (25.07%), Space: 45.2 MB (51.95%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canConstruct(String r, String m) {
+        HashMap<Character,Integer> hp = new HashMap<>();
+        
+        for(char ch : m.toCharArray())
+        {
+            hp.put(ch,hp.getOrDefault(ch,0)+1);
+
+        }
+        for(char ch : r.toCharArray())
+        {
+            if(!hp.containsKey(ch) || hp.get(ch)==0)
+            {
+                return false;
+            }
+            hp.put(ch,hp.get(ch)-1);
+        }
+        return true;
+    }
+}
