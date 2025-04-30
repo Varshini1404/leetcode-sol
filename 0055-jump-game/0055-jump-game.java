2371ec379d7a139a8008abf971ee1d10f66df1b0
Varshini S <varshinis.22cse@kongu.edu>
2025-04-30 14:35:12 +0530

    Time: 2 ms (81.82%), Space: 46.1 MB (21.66%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int max = 0;
+        int n = nums.length;
+
+        for(int i=0;i<n;i++)
+        {
+            if(i>max)
+            {
+                return false;
+            }
+            else{
+                max = Math.max(max,nums[i]+i);
+            }
+
+        }
+        return true;    
+    }
+}
