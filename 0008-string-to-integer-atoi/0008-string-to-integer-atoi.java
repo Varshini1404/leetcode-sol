9bf6fd1648ad9d97bc8f1cf8aa2985c4fc268079
Varshini S <varshinis.22cse@kongu.edu>
2025-05-07 22:40:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,40 +1,42 @@
 class Solution {
     public int myAtoi(String s) {
-        int i = 0;
-        int n = s.length();
+       int i = 0;
+       int n = s.length();
 
-        while(i<n && s.charAt(i)==' ')
-        {
-            i++;
-        }
+       while(i<n && s.charAt(i)==' ')
+       {
+        i++;
+       }
+       int p = 0,ne = 0;
+       if(i<n && s.charAt(i)=='+')
+       {
+        p++;
+        i++;
+       }
+       if(i<n && s.charAt(i)=='-')
+       {
+        ne++;
+        i++;
+       }
 
-        int p = 0,ne = 0;
-        if(i<n && s.charAt(i)=='+')
-        {
-            p++;
-            i++;
-        }
-        if(i<n && s.charAt(i)=='-')
-        {
-            ne++;
-            i++;
-        }
-        double a = 0;
+       double a = 0;
 
-        while(i<n && s.charAt(i)>='0'&&s.charAt(i)<='9')
-        {
-            a=a*10+(s.charAt(i)-'0');
-            i++;
-        }
-        if(ne>0)
-        {
-            a=-a;
-        }
-        if(p>0 && ne>0)
-        {
-            return 0;
-        }
-        return (int)a;
+       while(i<n && s.charAt(i)>='0' && s.charAt(i)<='9')
+       {
+        a = a*10 +(s.charAt(i)-'0');
+        i++;
+       }
+       if(ne>0)
+       {
+        a=-a;
+        
+        
+       }
+       if(p>0 && n>0)
+       {
+        return 0;
+       }
+       return (int)a;
         
     }
 }
