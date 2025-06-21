244b90ec75880553b04fbcb13ec30a5ded3a9273
Varshini S <varshinis.22cse@kongu.edu>
2025-06-21 23:42:28 +0530

    Time: 0 ms (100%), Space: 41.3 MB (41.06%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,21 +15,14 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p!= null && q!=null)
+        if(p == null && q == null)
         {
-            if(p.val==q.val)
-            {
-                return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
-            }
-                return false;
-            
-        }
-            else if(p!=null || q!=null)
-            {
-                return false;
-            }
             return true;
-        
-        
+        }
+         if(p==null || q == null|| p.val!=q.val)
+        {
+            return false;
+        }
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
     }
 }
