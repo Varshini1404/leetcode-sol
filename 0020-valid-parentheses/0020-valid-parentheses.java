6afae5e3adf3514ed13fc39eaab585ea7e245555
Varshini S <varshinis.22cse@kongu.edu>
2025-06-28 21:41:59 +0530

    Time: 2 ms (96.84%), Space: 42 MB (31.41%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,29 +1,26 @@
 import java.util.Stack;
-
 class Solution {
     public boolean isValid(String str) {
         Stack<Character> s = new Stack<>();
-
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-
-            if (c == '(' || c == '{' || c == '[') {
-                s.push(c);
-            } else {
-                if (s.isEmpty()) {
-                    return false;
-                }
-                char top = s.peek();
-                if ((top == '(' && c == ')') || 
-                    (top == '{' && c == '}') || 
-                    (top == '[' && c == ']')) {
-                    s.pop();
-                } else {
-                    return false;
-                }
-            }
+        for(int i=0;i<str.length();i++)
+        {
+        char c = str.charAt(i);
+        if(c=='{'||c=='('||c=='[')
+        {
+            s.push(c);
         }
+        else{
+            if(s.isEmpty())
+            return false;;
+        
+        char top = s.pop();
+        if((c==']'&& top!='[')||(c=='}'&&top!='{')||(c==')'&&top!='('))
+        {
 
+            return false;
+        }
+        }
+        }
         return s.isEmpty();
     }
 }
