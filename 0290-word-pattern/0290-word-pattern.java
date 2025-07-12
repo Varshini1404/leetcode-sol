41f95db76ca8f3e9e5e5ac2dba51ff8d90364c63
Varshini S <varshinis.22cse@kongu.edu>
2025-07-12 20:53:36 +0530

    Time: 0 ms (100%), Space: 41.9 MB (10.46%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,30 +1,33 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        String[] words = s.split(" ");
-        if (pattern.length() != words.length) return false;
+        String words[] = s.split(" ");
+        
+        if(pattern.length()!=words.length)
+        {
+            return false;
+        }
         HashMap<Character,String> hp = new HashMap<>();
         for(int i=0;i<pattern.length();i++)
         {
             char pc = pattern.charAt(i);
-            String w = words[i];
+            String sc = words[i];
         
         if(hp.containsKey(pc))
         {
-            if(!hp.get(pc).equals(w))
+            if(!hp.get(pc).equals(sc))
             {
                 return false;
             }
         }
-        
         else{
-            if(hp.containsValue(w))
+            if(hp.containsValue(sc))
             {
                 return false;
             }
-        
+            hp.put(pc,sc);
         }
-        hp.put(pc,w);
         }
         return true;
+
     }
 }
