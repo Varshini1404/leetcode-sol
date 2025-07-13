7a12033868ad25f87340f2727a3c98a4ccc7589e
Varshini S <varshinis.22cse@kongu.edu>
2025-07-13 20:59:45 +0530

    Time: 3 ms (50.85%), Space: 45.7 MB (71%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,17 +1,15 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int max = 0;
-        int n = nums.length;
-        for(int i=0;i<n;i++)
+        int max=0;
+        for(int i=0;i<nums.length;i++)
         {
-        if(i>max)
-        {
-            return false;
-        }
-        else{
-             max= Math.max(max,nums[i]+i);
-        }
-        }
-        return true;
+            if(i>max){
+                return false;
+            }
+            else{
+             max = Math.max(max,nums[i]+i);
+            }  
+    }
+    return true;
     }
 }
