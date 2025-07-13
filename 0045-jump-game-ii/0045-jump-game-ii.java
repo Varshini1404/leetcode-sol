10394c7f0fcdebb4a3a416753e5be311814477b8
Varshini S <varshinis.22cse@kongu.edu>
2025-07-13 22:52:36 +0530

    Time: 1 ms (99.41%), Space: 45.1 MB (60.94%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,17 +1,17 @@
 class Solution {
     public int jump(int[] nums) {
-        int j=0,far=0,end=0;
-        int n = nums.length;
-        for(int i=0;i<n-1;i++)
+        int jump=0;
+        int far=0;
+        int curr=0;
+        for(int i=0;i<nums.length-1;i++)
         {
-            far = Math.max(far,i+nums[i]);
-            if(i==end)
+           far=Math.max(far,nums[i]+i);
+            if(i==curr)
             {
-                j++;
-                end = far;
-                
+                jump++;
+                curr=far;
             }
         }
-        return j;
+        return jump;
     }
 }
